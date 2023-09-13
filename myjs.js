deb73f3572bc6f573d3fc37aa5c3036fbbe13fec
Sura Itana <141846629+surraaI@users.noreply.github.com>
2023-09-13 05:14:13 +0300

    uptodate

--- myjs.js
+++ myjs.js
@@ -5,13 +5,15 @@ function getComputerChoice() {
 }
 arr = ["rock", "scissor", "paper"]
 
-function Game(playerSelection, computerSelection) {
+function playround(playerSelection, computerSelection) {
     playerSelection = prompt(`paper,scissor, rock: `)
     playerSelection = playerSelection.toLowerCase()
     computerSelection = getComputerChoice()
     if (!arr.includes(playerSelection)) { alert(`wrong input`) }
-    if ((playerSelection === `rock` && computerSelection === `paper`) || (playerSelection === `paper` && computerSelection === `scissor`) || (playerSelection === `scissor` && computerSelection === `rock`)) { alert(`you lose the computer chose ${computerSelection}`) } else { alert(`you win the computer chose ${computerSelection}`) }
-
+    if (arr.includes(playerSelection)) {
+        if ((playerSelection === `rock` && computerSelection === `paper`) || (playerSelection === `paper` && computerSelection === `scissor`) || (playerSelection === `scissor` && computerSelection === `rock`)) { return `you lose the computer chose ${computerSelection}` } else { return `you win the computer chose ${computerSelection}` }
+    }
 
 }
-Game()
+playround()
+sonsole.log(playround())
