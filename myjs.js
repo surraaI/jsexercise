bdba82d6548e9947674f3fb74ab66ff298b386c8
Sura Itana <141846629+surraaI@users.noreply.github.com>
2023-09-13 05:02:04 +0300

    jj

--- myjs.js
+++ myjs.js
@@ -6,11 +6,16 @@ function getComputerChoice() {
 
 
 function Game(playerSelection, computerSelection) {
-    playerSelection = prompt(`paper,scissor, rock: `)
+    const playerSelection = prompt(`paper,scissor, rock: `)
     playerSelection = playerSelection.toLowerCase()
-    computerSelection = getComputerChoice()
-    if ((playerSelection === `rock` && computerSelection === `paper`) || (playerSelection === `paper` && computerSelection === `scissor`) || (playerSelection === `scissor` && computerSelection === `rock`)) { alert(`you lose the computer chose ${computerSelection}`) } else { alert(`you win the computer chose${computerSelection}`) }
+    const computerSelection = getComputerChoice()
+    if ((playerSelection === `rock` && computerSelection === `paper`) || (playerSelection === `paper` && computerSelection === `scissor`) || (playerSelection === `scissor` && computerSelection === `rock`)) { alert(`you lose the computer chose ${computerSelection}`) } else { alert(`you win the computer chose ${computerSelection}`) }
 
 
 }
 Game()
+console.log(Game())
+console.log(Game())
+console.log(Game())
+console.log(Game())
+console.log(Game())
