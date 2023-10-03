eb633f3bbead2556e2537ef191fcb9924caa8239
Sura Itana <141846629+surraaI@users.noreply.github.com>
2023-10-03 13:53:42 +0300

    new

--- myjs.js
+++ myjs.js
@@ -4,20 +4,66 @@ function getComputerChoice() {
 
 }
 arr = ["rock", "scissor", "paper"]
+let count = 0;
+let mark = 0
+let div = document.getElementsByTagName('div');
+let i = 0;
 
-function playround(playerSelection, computerSelection) {
-    playerSelection = prompt(`paper,scissor, rock: `)
-    playerSelection = playerSelection.toLowerCase()
-    computerSelection = getComputerChoice()
-    if (!arr.includes(playerSelection)) { alert(`wrong input`) }
-    if (arr.includes(playerSelection)) {
-        if ((playerSelection === `rock` && computerSelection === `paper`) || (playerSelection === `paper` && computerSelection === `scissor`) || (playerSelection === `scissor` && computerSelection === `rock`)) { return `you lose the computer chose ${computerSelection} you chose ${playerSelection}` } else if (playerSelection === computerSelection) { return `draw` } else { return `you win the computer chose ${computerSelection} you chose ${playerSelection}` }
+function five() {
+    if (count <= 4) { count += 1 } else if (count === 5) {
+
+        if (mark > 0) {
+            alert('you win')
+        } else if (mark === 0) { alert(`you draw`) } else { alert(`you lose`) }
+
+
+        for (j = 0; j < 5; j++) { div[j].innerHTML = '' }
+        i = 0
+        count = 0
     }
 
 }
 
-console.log(playround())
-console.log(playround())
-console.log(playround())
-console.log(playround())
-console.log(playround())
+
+function playround(playerSelection) {
+
+
+
+    playerSelection = playerSelection.toLowerCase()
+    computerSelection = getComputerChoice()
+    if (!arr.includes(playerSelection)) { alert(`wrong input`) }
+    if (arr.includes(playerSelection)) {
+        if ((playerSelection === `rock` && computerSelection === `paper`) || (playerSelection === `paper` && computerSelection === `scissor`) || (playerSelection === `scissor` && computerSelection === `rock`)) {
+            div[i].innerHTML = `you lose the computer chose ${computerSelection} you chose ${playerSelection}`;
+            mark -= 1;
+            i += 1
+        } else if (playerSelection === computerSelection) {
+            div[i].innerHTML = `draw`;
+            i += 1
+        } else {
+            div[i].innerHTML = `you win the computer chose ${computerSelection} you chose ${playerSelection}`;
+            mark += 1
+            i += 1
+        }
+    }
+    five();
+}
+
+
+const paper = document.getElementById('paper');
+const scissor = document.getElementById('scissor');
+const stone = document.getElementById('stone');
+
+paper.onclick = function() {
+    playround('paper');
+
+
+};
+scissor.onclick = function() {
+    playround('scissor');
+
+};
+stone.onclick = function() {
+    playround('rock');
+
+};
